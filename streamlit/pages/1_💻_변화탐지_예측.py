6e0ad5b1efb085072f5210de39d51b3bed5a5061
Ojung-ii <ojh7839@gmail.com>
2023-11-08 15:48:59 +0900

    add Proceed button

--- streamlit/pages/1_💻_변화탐지_예측.py
+++ streamlit/pages/1_💻_변화탐지_예측.py
@@ -3,44 +3,55 @@ import folium
 from streamlit_folium import folium_static
 import json
 
-
-# page setting and title
-st.set_page_config(page_title="변화탐지_예측", page_icon="👀")
+# 페이지 설정과 제목
+st.set_page_config(page_title="변화탐지_예측", page_icon="👀", layout="wide")
 st.title("변화탐지 예측")
 st.write("---"*20)
 
-
-# 'aoi.geojson' file load
-with open('aoi.geojson', 'r', encoding= "utf-8") as f:
+# 'aoi.geojson' 파일 로드
+with open('aoi.geojson', 'r', encoding="utf-8") as f:
     geojson_data = json.load(f)
 
-# aoi list
+# 관심 지역 목록
 area_names = [feature['properties']['name'] for feature in geojson_data['features']]
+area_names.append("새로운 관심영역 넣기")  # 드롭다운 목록에 새 옵션 추가
 
-# divide section
-col1,col2 = st.columns([0.8,0.3])
+# 섹션 나누기
+col1, col2 = st.columns([0.7, 0.3])
 
+# aoi 초기화
+aoi = None
 
-# right section : choice of input
+# 오른쪽 섹션: 입력 선택
 with col2:
-    
-    # aoi selection
+    # 관심 지역 선택
     selected_name = st.selectbox("관심 지역을 선택하세요:", area_names)
+    
+    # '새로운 관심영역 넣기'가 선택되면 파일 업로드 기능 활성화
+    if selected_name == "새로운 관심영역 넣기":
+        uploaded_file = st.file_uploader("GeoJSON 파일을 업로드하세요", type=['geojson'])
+        if uploaded_file is not None:
+            # 파일 읽기
+            aoi = json.load(uploaded_file)
+    else:
+        # 기존 관심 지역 선택
+        aoi = next((feature for feature in geojson_data['features'] if feature['properties']['name'] == selected_name), None)
 
-    # choose one aoi
-    aoi = next((feature for feature in geojson_data['features'] if feature['properties']['name'] == selected_name), None)
+    # 날짜 선택
+    start_date = st.date_input('시작날짜 선택하세요:')  # 디폴트로 오늘 날짜가 찍혀 있다.
+    end_date = st.date_input('끝날짜 선택하세요:')    # 디폴트로 오늘 날짜가 찍혀 있다.
 
-    # select date
-    start_date = st.date_input('시작날짜 선택하세요:') # 디폴트로 오늘 날짜가 찍혀 있다.
-    end_date = st.date_input('끝날짜 선택하세요:') # 디폴트로 오늘 날짜가 찍혀 있다.
-
-
-# left section : visualize mapping with polygon
+    # 분석 실행 버튼
+    st.write("")
+    proceed_button = st.button("☑️ 분석 실행")
+    
+    
+# 왼쪽 섹션: 폴리곤 매핑 시각화
 with col1:
     # 지도 초기화 (대한민국 중심 위치로 설정)
     m = folium.Map(location=[36.5, 127.5], zoom_start=7)
-    
-   # 선택된 관심 지역이 있을 경우에만 해당 지역 폴리곤 표시
+
+    # 선택된 관심 지역이 있을 경우에만 해당 지역 폴리곤 표시
     if aoi:
         folium.GeoJson(
             aoi,
@@ -50,11 +61,8 @@ with col1:
         # 지도를 선택된 폴리곤에 맞게 조정
         m.fit_bounds(folium.GeoJson(aoi).get_bounds())
 
-
     # Streamlit 앱에 지도 표시
     folium_static(m)
 
-
-
-# 여기는 그래프 넣기
+# 그래프 영역
 st.write("PETER's CODE HERE for Graph~~~~")
--- streamlit/pages/2_⏱️_시계열_경향성_분석.py
+++ streamlit/pages/2_⏱️_시계열_경향성_분석.py
@@ -2,43 +2,58 @@ import streamlit as st
 import folium
 from streamlit_folium import folium_static
 import json
-import ee
-import pandas as pd
-from prophet import Prophet
 import sar_func
 
-start_date = '2017-01-01'
-end_date = '2023-03-31'
 
-# page setting and title
-st.set_page_config(page_title="변화탐지_예측", page_icon="👀")
+# 페이지 설정과 제목
+st.set_page_config(page_title="시계열 경향성 분석", page_icon="⏱️", layout = 'wide')
 st.title("⏱️ 시계열 경향성 분석")
 st.write("---"*20)
 
-# 'aoi.geojson' file load
+# 'aoi.geojson' 파일 로드
 with open('aoi.geojson', 'r', encoding="utf-8") as f:
     geojson_data = json.load(f)
 
-# aoi list
+# 관심 지역 목록
 area_names = [feature['properties']['name'] for feature in geojson_data['features']]
+area_names.append("새로운 관심영역 넣기")  # 드롭다운 목록에 새 옵션 추가
 
-# divide section
-col1, col2 = st.columns([0.8, 0.3])
+# 섹션 나누기
+col1, col2 = st.columns([0.7, 0.3])
 
-# right section : choice of input
+# aoi 초기화
+aoi = None
+
+# 오른쪽 섹션: 입력 선택
 with col2:
-    # aoi selection
+    # 관심 지역 선택
     selected_name = st.selectbox("관심 지역을 선택하세요:", area_names)
     
-    # choose one aoi
-    aoi = next((feature for feature in geojson_data['features'] if feature['properties']['name'] == selected_name), None)
+    # '새로운 관심영역 넣기'가 선택되면 파일 업로드 기능 활성화
+    if selected_name == "새로운 관심영역 넣기":
+        uploaded_file = st.file_uploader("GeoJSON 파일을 업로드하세요", type=['geojson'])
+        if uploaded_file is not None:
+            # 파일 읽기
+            aoi = json.load(uploaded_file)
+    else:
+        # 기존 관심 지역 선택
+        aoi = next((feature for feature in geojson_data['features'] if feature['properties']['name'] == selected_name), None)
 
-# left section : visualize mapping with polygon
-with col1:
-    # 지도 초기화 (대한민국 중심 위치로 설정)x``
-    m = folium.Map(location=[36.5, 127.5], zoom_start=7)
+    # 날짜 선택
+    start_date = st.date_input('시작날짜 선택하세요:').strftime('%Y-%m-%d')   # 디폴트로 오늘 날짜가 찍혀 있다.
+    end_date = st.date_input('끝날짜 선택하세요:').strftime('%Y-%m-%d')     # 디폴트로 오늘 날짜가 찍혀 있다.
+
     
-   # 선택된 관심 지역이 있을 경우에만 해당 지역 폴리곤 표시
+    # 분석 실행 버튼
+    st.write("")
+    proceed_button = st.button("☑️ 분석 실행")
+    
+# 왼쪽 섹션: 폴리곤 매핑 시각화
+with col1:
+    # 지도 초기화 (대한민국 중심 위치로 설정)
+    m = folium.Map(location=[36.5, 127.5], zoom_start=7)
+
+    # 선택된 관심 지역이 있을 경우에만 해당 지역 폴리곤 표시
     if aoi:
         folium.GeoJson(
             aoi,
@@ -48,14 +63,16 @@ with col1:
         # 지도를 선택된 폴리곤에 맞게 조정
         m.fit_bounds(folium.GeoJson(aoi).get_bounds())
 
-
     # Streamlit 앱에 지도 표시
     folium_static(m)
-
 # 여기는 그래프 넣기
 # st.write("STEVE's CODE HERE for Graph~~~~")
 # st.write(aoi)
-if aoi:
+if proceed_button:
+    st.write("-----"*20)
+    st.markdown("""
+        <h3 style='text-align: center; font-size: 30px;'>⬇️ 시계열 분석 결과 </h3>
+        """, unsafe_allow_html=True)
     parse_aoi = sar_func.create_ee_polygon_from_geojson(aoi)
     start_date = '2017-01-01'
     end_date = '2023-03-01'
