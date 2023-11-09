5f14e767c55926a51b334ffdfcee987341987db8
Ojung-ii <ojh7839@gmail.com>
2023-11-09 09:59:19 +0900

    타임랩스 페이지 수정

--- streamlit/pages/3_👀_타임랩스.py
+++ streamlit/pages/3_👀_타임랩스.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import geemap
 import ee
+from timelapse_func import create_sentinel1_timelapse, create_sentinel2_timelapse
+import json
+from sar_func import create_ee_polygon_from_geojson
 
 # Google Earth Engine 초기화
 ee.Initialize()
@@ -8,7 +11,7 @@ ee.Initialize()
 # Streamlit 앱 제목 설정
 st.set_page_config(page_title="변화탐지_확인", page_icon="👀")
 
-st.title('Sentinel-1 타임랩스 생성기')
+st.title('👀 타임랩스 생성기')
 st.write("---"*20)
 # 날짜 형식 변환 함수
 def format_date(date_int):
@@ -16,76 +19,47 @@ def format_date(date_int):
     # YYYYMMDD 형식의 문자열을 YYYY-MM-DD로 변환
     return f'{date_str[:4]}-{date_str[4:6]}-{date_str[6:]}'
 
-# 사용자 입력 받기
+# 'aoi.geojson' 파일 로드
+with open('aoi.geojson', 'r', encoding="utf-8") as f:
+    geojson_data = json.load(f)
+
+# 관심 지역 목록
+area_names = [feature['properties']['name'] for feature in geojson_data['features']]
+area_names.append("새로운 관심영역 넣기")  # 드롭다운 목록에 새 옵션 추가
+
+
+# User's Input
+dataset = st.selectbox('데이터셋 선택', ['Sentinel-1', 'Sentinel-2'])
+selected_name = st.selectbox("관심 지역을 선택하세요:", area_names)
 start_date = st.text_input('시작 날짜 (YYYYMMDD 형식)', value='20200101')
 end_date = st.text_input('종료 날짜 (YYYYMMDD 형식)', value='20200131')
 frequency = st.selectbox('빈도 선택', options=['day', 'month', 'quarter', 'year'])
+
+
+# '새로운 관심영역 넣기'가 선택되면 파일 업로드 기능 활성화
+if selected_name == "새로운 관심영역 넣기":
+    uploaded_file = st.file_uploader("GeoJSON 파일을 업로드하세요", type=['geojson'])
+    if uploaded_file is not None:
+        # 파일 읽기
+        aoi = json.load(uploaded_file)
+else:
+    # 기존 관심 지역 선택
+    aoi = next((feature for feature in geojson_data['features'] if feature['properties']['name'] == selected_name), None)
+    
+    aoi = create_ee_polygon_from_geojson(aoi)
 
 # 사용자가 제공한 날짜를 변환
 formatted_start_date = format_date(int(start_date))
 formatted_end_date = format_date(int(end_date))
 
-# Taeanhaean의 ROI 설정
-roi = ee.FeatureCollection("WCMC/WDPA/current/polygons") \
-    .filter(ee.Filter.eq("NAME", "Sinduri Sand Dune Water")) \
-    .geometry().bounds()
 
-# 타임랩스 생성 버튼
 if st.button('타임랩스 생성'):
     with st.spinner('타임랩스를 생성하는 중입니다...'):
-        try:
-            # 타임랩스 생성 로직
-            output_gif = './timelapse.gif'  # 타임랩스를 저장할 경로와 파일명
-            
-            # geemap.sentinel1_timelapse 함수 호출
-            timelapse = geemap.sentinel1_timelapse(
-                roi=roi,
-                out_gif=output_gif,
-                start_year=int(start_date[:4]),
-                end_year=int(end_date[:4]),
-                start_date=f"{start_date[4:6]}-{start_date[6:]}",  
-                end_date=f"{end_date[4:6]}-{end_date[6:]}",
-                frequency=frequency,
-                date_format=None,
-                palette='Greys',
-                vis_params=None,
-                dimensions=768,
-                frames_per_second=1,
-                crs='EPSG:3857',
-                overlay_data=None,
-                overlay_color='black',
-                overlay_width=1,
-                overlay_opacity=1.0,
-                title=None,
-                title_xy=('2%', '90%'),
-                add_text=True,
-                text_xy=('2%', '2%'),
-                text_sequence=None,
-                font_type='arial.ttf',
-                font_size=20,
-                font_color='white',
-                add_progress_bar=True,
-                progress_bar_color='blue',
-                progress_bar_height=5,
-                add_colorbar=False,
-                colorbar_width=6.0,
-                colorbar_height=0.4,
-                colorbar_label=None,
-                colorbar_label_size=12,
-                colorbar_label_weight='normal',
-                colorbar_tick_size=10,
-                colorbar_bg_color=None,
-                colorbar_orientation='horizontal',
-                colorbar_dpi='figure',
-                colorbar_xy=None,
-                colorbar_size=(300, 300),
-                loop=0, mp4=False,
-                fading=False,
-                orbitProperties_pass='DESCENDING'
-            )
-            
-            # 타임랩스 이미지 표시
-            st.image(output_gif, caption='Sentinel-1 타임랩스', use_column_width=True)
+        output_gif = './timelapse.gif'  # 타임랩스를 저장할 경로와 파일명
+        if dataset == 'Sentinel-1':
+            create_sentinel1_timelapse(aoi, start_date, end_date, frequency, output_gif)
+        elif dataset == 'Sentinel-2':
+            create_sentinel2_timelapse(aoi, start_date, end_date, frequency, output_gif)
 
-        except Exception as e:
-            st.error(f'타임랩스 생성 중 오류 발생: {e}')
+        st.image(output_gif, caption=f'{dataset} 타임랩스', use_column_width=True)
+
