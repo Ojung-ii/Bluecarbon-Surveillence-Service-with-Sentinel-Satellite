4eee27c5dd766d0a1f8da95f61bf788f260ddf1e
Ojung-ii <ojh7839@gmail.com>
2023-11-16 17:01:39 +0900

    .

--- streamlit/subpage/aoi_revision.py
+++ streamlit/subpage/aoi_revision.py
@@ -67,8 +67,8 @@ def app():
                     st.error("선택된 관심 영역을 찾을 수 없습니다.")
                 # Streamlit 앱에 지도 표시
 
-                
             folium_static(m)
+
         # 신규 AOI 추가 탭
         with tab2:
             st.subheader("신규 AOI 추가")
@@ -115,7 +115,9 @@ def app():
                 st.success(f"'{selected_aoi_name_to_remove}' 이름의 AOI가 성공적으로 제거되었습니다.")
                 aoi_names.remove(selected_aoi_name_to_remove)  # 업데이트된 aoi_names 리스트
                 
-            
+
 # launch
 if __name__  == "__main__" :
     app()
+    
+    
