5464f753943b504db6210797e1258efd959cbb14
Mujae <38150072+Mujae@users.noreply.github.com>
2023-11-29 00:47:46 +0900

    그냥커밋

--- streamlit/main.py
+++ streamlit/main.py
@@ -20,7 +20,7 @@ def launch() :
         st.image("logo/bluecheck_title_logo.png")
     # sub_title
     st.markdown("""
-        <h3 style='text-align: center; font-size: 30px;'> SAR를 활용한 블루카본 변화탐지 서비스 </h3>
+        <h3 style='text-align: center; font-size: 30px;'> 위성영상을 활용한 블루카본 변화탐지 서비스 </h3>
         """, unsafe_allow_html=True)
     
     st.write("-------"*20)
