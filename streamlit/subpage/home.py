49e4d73682210497a48f8c6026f72ef04a50cfc2
gunwoda <gunwo3442@gmail.com>
2024-01-02 19:44:53 +0900

    1111

--- streamlit/subpage/home.py
+++ streamlit/subpage/home.py
@@ -6,19 +6,19 @@ def app():
     empty1,col1,col2,col3,col4,empty2 = st.columns([0.5,0.3,0.3,0.3,0.3,0.5], gap="small")
     
     with col1: # KNPS Logo
-        st.image("logo/knps_logo.png")
+        st.image("streamlit/logo/knps_logo.png")
     with col2: # BigLeader Logo
-        st.image("logo/bigleader_logo.png")
+        st.image("streamlit/logo/bigleader_logo.png")
     with col3: # Google Logo
-        st.image("logo/google_logo.png")
+        st.image("streamlit/logo/google_logo.png")
     with col4: # Meta Logo
-        st.image("logo/meta_logo.png")
+        st.image("streamlit/logo/meta_logo.png")
 
 
     # Logo timelapse
     empty3,col5,empty4 = st.columns([0.3,0.5,0.3])
     with col5 : 
-        st.image("logo/bluecheck_mainpage_logo.gif",  use_column_width="always")
+        st.image("streamlit/logo/bluecheck_mainpage_logo.gif",  use_column_width="always")
 
 # launch
 if __name__  == "__main__" :
